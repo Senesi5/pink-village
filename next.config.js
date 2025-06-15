2c3879c5041e338068b04d6d2641a5c26022c22f
Senesi5 <andrea.senesi@proton.me>
2025-06-15 10:18:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,3 +6,12 @@ const nextConfig = {
     domains: ['qxyvkcuodputbnmajzsl.supabase.co'],
     unoptimized: true
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
